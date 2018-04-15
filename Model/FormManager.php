b9ef4cfc7a239730d8950e100a9bb0cf33d7f0c6
Karla Og <karla@macbook-pro-de-karla.home>
2018-04-15 17:44:36 +0200

    started login et register

---
+++ Model/FormManager.php
@@ -0,0 +1,22 @@
+<?php 
+namespace Model;
+use Model\UserManager;
+class FormManager
+{
+    public function checkLogin($username, $password)
+    {
+        if (empty($password) OR empty($username)) {
+            return false;
+        }
+        $userManager = new UserManager();
+        $usersByUsername = $userManager->getUserByUsername($username);
+        if ($password == $usersByUsername['password']) {
+            return $usersByUsername['user_id'];
+        } else if (empty($usersByUsername)) {
+            return false;
+        } else {
+            return false;
+        }
+    }
+}
+?>
