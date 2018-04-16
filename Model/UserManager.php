ad630bd983484ad51b387038dc3deb110a7da559
Karla Og <karla@macbook-pro-de-karla.home>
2018-04-16 17:18:33 +0200

    started login / register

--- Model/UserManager.php
+++ Model/UserManager.php
@@ -1,77 +1,67 @@
 <?php
 namespace Model;
 use Cool\DBManager;
-class UserManager
+
+class FormManager
 {
-    public function getUserByUsername($username)
+    public function CheckUsername($username)
     {
         $dbm = DBManager::getInstance();
         $pdo = $dbm->getPdo();
-        $result = $pdo->prepare('SELECT * FROM users WHERE username = :username');
-        $result->execute([':username' => $username]);
-        $users = $result->fetch();
-        return $users;
-    }
-    public function login($username, $password)
-    {
-        if (empty($password) OR empty($username)) {
-            return false;
+        $result = $pdo->query("SELECT username FROM users");
+        var_dump($result); 
+        $posts = $result->fetchAll(PDO::FETCH_COLUMN, 0);
+        foreach ($posts as $value) {
+            if ($value === $username) {
+                return false;
+            }
         }
-        $salt = 'gvu.site';
-        $pw_hash = md5($salt . $password);
-        $userManager = new UserManager();
-        $usersByUsername = $userManager->getUserByUsername($username);
-        if ($pw_hash == $usersByUsername['password']) {
+        return true;
+    }
+    public function CheckPassword($username, $password)
+    {
+        $dbm = DBManager::getInstance();
+        $pdo = $dbm->getPdo();
+        $result = $pdo->query("SELECT password FROM user where username = '$username'");
+        $posts = $result->fetch(PDO::FETCH_COLUMN, 0);
+        if ($posts === $password) {
             $_SESSION['username'] = $username;
-            $_SESSION['id'] = $usersByUsername['user_id'];
             return true;
-        } else if (empty($usersByUsername)) {
-            return false;
+        }
+        return false;
+    }
+    public function Register($firstname, $lastname, $username, $email, $password, $password_repeat)
+    {
+        $dataerrors = [];
+        if (strlen($username) < 4) {
+            array_push($dataerrors, "4 letters needed for an username");
+        }
+        if (strlen($firstname) < 1) {
+            array_push($dataerrors, "Enter a first name");
+        }
+        if (strlen($lastname) < 1) {
+            array_push($dataerrors, "Enter a last name");
+        }
+        if (strlen($password) < 6) {
+            array_push($dataerrors, "Password too short(min6)");
+        }
+        if ($password !== $password_repeat) {
+            array_push($dataerrors, "Passwords does not match");
+        }
+        if ($dataerrors !== []) {
+            return $dataerrors;
         } else {
-            return false;
-        }
-    }
-    public function logout()
-    {
-        session_destroy();
-    }
-    public function register($username, $email, $password, $passwordRepeat)
-    {
-        $errors = [];
-        if (strlen($username) < 4 OR strlen($username) > 30) {
-            $errors[] = 'Le nom d\'utilisateur choisit est trop petit ou trop grand';
-        }
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $errors[] = 'Email invalide';
-        }
-        if (strlen($email) > 250) {
-            $errors[] = 'L\'email choisit est trop grand';
-        }
-        if (strlen($password) < 4 || strlen($password) > 30) {
-            $errors[] = 'Le mot de passe choisit est trop petit ou trop grand';
-        }
-        if ($password !== $passwordRepeat) {
-            $errors[] = 'Les deux mots de passe doivent être les mêmes';
-        }
-        if (empty($errors)) {
+
             $dbm = DBManager::getInstance();
             $pdo = $dbm->getPdo();
-            $salt = 'gvu.site';
-            $pw_hash = md5($salt . $password);
-            $creation = date('Y-m-d H:i:s');
-            $role = "user";
-            $tokenPassword = bin2hex(openssl_random_pseudo_bytes(16));
-            $stmt = $pdo->prepare("INSERT INTO `users` (`user_id`, `username`, `email`, `password`, `creation`, `role`, `token_password`) VALUES (NULL, :username, :email, :password, :creation, :role, :tokenPassword);");
-            $stmt->bindParam(':username', $username);
-            $stmt->bindParam(':email', $email);
-            $stmt->bindParam(':password', $pw_hash);
-            $stmt->bindParam(':creation', $creation);
-            $stmt->bindParam(':role', $role);
-            $stmt->bindParam(':tokenPassword', $tokenPassword);
-            $stmt->execute();
+            $result = $pdo->prepare('INSERT INTO `user` (`id`, `firstname`, `lastname`,`username`, `email`, `password`, `creation` ) VALUES (NULL, :firstname, :lastname, :username ,:email  ,:password ,:creation )');
+            $result->bindParam(':firstname', $firstname);
+            $result->bindParam(':lastname', $lastname);
+            $result->bindParam(':email', $email);
+            $result->bindParam(':password', $password);
+            $result->bindParam(':username', $username);
+            $result->execute();
             return true;
-        } else {
-            return $errors;
         }
     }
 }
