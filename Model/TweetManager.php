7991d163b5868b4be61a83205d2b1c1a93670bb8
Emeline <emeline.garo@supinternet.fr>
2018-05-13 16:06:38 +0200

    fixed errors

--- Model/TweetManager.php
+++ Model/TweetManager.php
@@ -11,22 +11,13 @@ class TweetManager extends BaseManager
     {
 
         $pdo = $this->setPdo();
-        $sth = $pdo->prepare('SELECT users.firstname,users.id, posts.user,posts.content FROM users RIGHT JOIN posts ON posts.user = users.id ORDER BY creation_date DESC ');
+        $sth = $pdo->prepare('SELECT users.firstname,users.id, posts.user,posts.content FROM users LEFT JOIN posts ON posts.user = users.id ORDER BY creation_date DESC  ');
         $sth->execute();
         // $sth->execute([$id]);
         $comments = $sth->fetchAll();
         return $comments;
     }
 
-//    public function addTweet()
-//    {
-//        $pdo = $this->setPdo();
-//        $stmt = $pdo->prepare('INSERT INTO posts(id,content,creation_date) VALUES (NULL,?, NOW())');
-//      //  $stmt->execute();
-//        //$comments = $sth->fetchAll();
-//        $stmt->execute();
-//        return $stmt;
-//    }
     public function addTweet($user, $content)
     {
         $pdo = $this->setPdo();
@@ -40,6 +31,8 @@ class TweetManager extends BaseManager
         $stmt->execute();
         return $stmt;
     }
+
+
 
 }
 
