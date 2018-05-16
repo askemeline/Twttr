09895b54b8b4b9bb4a32cbce48952ce58c10a06d
Karla Og <karla@macbook-pro-de-karla.home>
2018-05-16 20:53:39 +0200

    reaction manager changed

--- Model/ReactionManager.php
+++ Model/ReactionManager.php
@@ -7,7 +7,7 @@ class ReactionManager extends BaseManager
     public function likePosts()
     {
         $pdo = $this->setPdo(); 
-        $stmt = $pdo->prepare('SELECT favs , username_id FROM favs INNER JOIN users');
+        $stmt = $pdo->prepare('SELECT favs.* , username_id FROM favs INNER JOIN users');
         $sth->bindParam(':user_id', $_SESSION['u_id']); 
 
 
