2396a2ec8746fc7f1f147c96cc667febb7b63d44
Karla Og <karla@macbook-pro-de-karla.home>
2018-05-16 22:50:27 +0200

    final commit

--- Model/ReactionManager.php
+++ Model/ReactionManager.php
@@ -7,7 +7,10 @@ class ReactionManager extends BaseManager
     public function likePosts()
     {
         $pdo = $this->setPdo(); 
-        $stmt = $pdo->prepare('SELECT favs.* , username_id FROM favs INNER JOIN users');
+        $stmt = $pdo->prepare('INSERT INTO favs(user, posts) VALUES (:user,:posts)');
+        $stmt = $pdo->prepare('SELECT favs.* , username_id FROM favs ');
+
+
         $sth->bindParam(':user_id', $_SESSION['u_id']); 
 
 
